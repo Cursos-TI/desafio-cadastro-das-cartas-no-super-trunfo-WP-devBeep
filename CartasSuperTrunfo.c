005f49932b0914c689caa4f837f62ed9366cb010
WP-devBeep <wagnercp97@gmail.com>
2025-02-28 01:23:53 +0000

    criei as variaveis card1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    // variáveis para armazenamento dos dados da carta número 1.
+    char estado1;
+    char codigo1[10];
+    char cidade1[50];
+    int populaçao1;
+    float area1;
+    float pib1;
+    int pTuristicos1;
+
+    // variáveis para armazenamento dos dados da carta número 2.
+    char estado2;
+    char codigo2[10];
+    char cidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pTuristicos2;
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // comandos para solicitação e impressao dos dados inseridos pelo usuário, para a carta número1.
+    printf("insira o estado: \n");
+    scanf("%s", &estado1);
+
+    printf("insira o codigo da carta: \n");
+    scanf("%s", &codigo1);
+
+    printf("insira a cidade: \n");
+    scanf("%s", &cidade1);
+
+    printf("insira a população: \n");
+    scanf("%d", &populaçao1);
+
+    printf("insira a área da cidade: \n");
+    scanf("%f", &area1);
+
+    printf("insira o PIB: \n");
+    scanf("%f", &pib1);
+
+    printf("insira o número de pontos turísticos: \n");
+    scanf("%d", &pTuristicos1);
 
     return 0;
 }
