829b03458724a630ec5d308e347215169a036015
WP-devBeep <wagnercp97@gmail.com>
2025-03-06 01:50:56 +0000

    comando printf card1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,16 +2,16 @@
 
 int main() {
     // variáveis para armazenamento dos dados da carta número 1.
-    char estado1;
+    char estado1[20];
     char codigo1[10];
     char cidade1[50];
-    int populaçao1;
+    int populacao1;
     float area1;
     float pib1;
     int pTuristicos1;
 
     // variáveis para armazenamento dos dados da carta número 2.
-    char estado2;
+    char estado2[20];
     char codigo2[10];
     char cidade2[50];
     int populacao2;
@@ -30,7 +30,7 @@ int main() {
     scanf("%s", &cidade1);
 
     printf("insira a população: \n");
-    scanf("%d", &populaçao1);
+    scanf("%d", &populacao1);
 
     printf("insira a área da cidade: \n");
     scanf("%f", &area1);
@@ -40,6 +40,14 @@ int main() {
 
     printf("insira o número de pontos turísticos: \n");
     scanf("%d", &pTuristicos1);
+
+    printf("estado: %s\n", estado1);
+    printf("código: %s\n", codigo1);
+    printf("cidade: %s\n", cidade1);
+    printf("população: %d\n", populacao1);
+    printf("área: %f km²\n", area1);
+    printf("PIB: %f bilhões de reais\n", pib1);
+    printf("pontos turísticos: %d\n", pTuristicos1);
 
     return 0;
 }
